77d3238582cf0800b8a2d3f172e49f476e1d50f1
Luke <luke.watts@workvivo.com>
2023-04-19 07:35:15 +0100

    Restructuring to reduce cognitive complexity. Adding comments and docblocks

--- src/createTheme.tsx
+++ src/createTheme.tsx
@@ -12,45 +12,67 @@ type ReplacementMapObject = {
     [key: string]: string;
 };
 
+/**
+ * Compile Theme
+ * Relaces all @key values with the value of the key
+ * 
+ * @author Luke Watts
+ * @since 0.1.0
+ * 
+ * @param compileTheme: ThemeObject
+ * @returns ThemeObject
+ */
+const compileTheme = (compileTheme: ThemeObject): ThemeObject => {
+    // 1. Loop over each property in the theme object
+    // 2. Split all the values by space
+    // 3. Loop over each value
+    // 4. If the value starts with @, create a property with the value as the key and the value as the value, e.g. { '@key': "bg-blue-500" }
+    const replacementMap: ReplacementMapObject = {};
+    for (const [, value] of Object.entries(compileTheme)) {
+        const classNames = value.split(" ");
+        for (const className of classNames) {
+            if (className.startsWith("@")) {
+                if (!replacementMap[className]) {
+                    const keyLookup = className.replace("@", "");
+                    replacementMap[className] = compileTheme[keyLookup];
+                }
+            }
+        }
+    }
+
+    // Create the compiled theme object, which will be what is re-compiled and returned
+    // This also means the compileTheme object is immutable. compiledTheme is a deep copy of compileTheme
+    const compiledTheme: ThemeObject = {};
+    for (const [key, value] of Object.entries(compileTheme)) {
+        compiledTheme[key] = value;
+    }
+
+    // 1. Loop over each property in the replacementMap object
+    // 2. Loop over each property in the compiledTheme object
+    // 3. Replace all instances of the key in the compileTheme value with the value from the replacementMap
+    for (const [replacementKey, replacementValue] of Object.entries(replacementMap)) {
+        for (const [key] of Object.entries(compiledTheme)) {
+            const replacedValue = compiledTheme[key].replace(replacementKey, replacementValue).replace(replacementKey, replacementValue);
+            compiledTheme[key] = replacedValue;
+        }
+    }
+    
+    return compiledTheme;
+};
+
+/**
+ * Create Theme
+ * Generated the useTheme hook to be called in the component
+ * 
+ * @author Luke Watts
+ * @since 0.0.1
+ * 
+ * @param customTheme: ThemeObject
+ * @returns Function
+ */
 export const createTheme = (customTheme: ThemeObject): Function => {
     return (): ThemeReturnedObject => {
         const [theme, setTheme] = useState(customTheme);
-
-        const compileTheme = (compileTheme: ThemeObject) => {
-            // 1. Loop over each property in the theme object
-            // 2. Split all the values by space
-            // 3. Loop over each value
-            // 4. If the value starts with @, create a property with the value as the key and the value as the value, e.g. { '@key': "bg-blue-500" }
-            const replacementMap: ReplacementMapObject = {};
-            for (const [, value] of Object.entries(compileTheme)) {
-                const classNames = value.split(" ");
-                for (const className of classNames) {
-                    if (className.startsWith("@")) {
-                        if (!replacementMap[className]) {
-                            const keyLookup = className.replace("@", "");
-                            replacementMap[className] = compileTheme[keyLookup];
-                        }
-                    }
-                }
-            }
-
-            // 1. Loop over each property in the replacementMap object
-            // 2. Loop over each property in the compileTheme object
-            // 3. Replace all instances of the key in the compileTheme value with the value from the replacementMap
-            const compiledTheme: ThemeObject = {};
-            for (const [key, value] of Object.entries(compileTheme)) {
-                compiledTheme[key] = value;
-            }
-
-            for (const [replacementKey, replacementValue] of Object.entries(replacementMap)) {
-                for (const [key] of Object.entries(compiledTheme)) {
-                    const replacedValue = compiledTheme[key].replace(replacementKey, replacementValue).replace(replacementKey, replacementValue);
-                    compiledTheme[key] = replacedValue;
-                }
-            }
-            
-            return compiledTheme;
-        };
 
         useEffect(() => {
             setTheme((_theme: ThemeObject) => {
